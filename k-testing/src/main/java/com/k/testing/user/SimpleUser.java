0eb1f016909b0eab829246643b9fe9e8b22cc084
TomaszBury <tomasz.bury+github@gmail.com>
2017-10-11 19:50:03 +0200

    ZADANIE.6.3

--- k-testing/src/main/java/com/k/testing/user/SimpleUser.java
+++ k-testing/src/main/java/com/k/testing/user/SimpleUser.java
@@ -1,12 +1,17 @@
 package com.k.testing.user;
 
 public class SimpleUser {
-    private String username;
-
-    public SimpleUser(String username) {
+    String username;
+    String realName;
+    public SimpleUser(String username, String realName) {
         this.username = username;
+        this.realName = realName;
     }
     public String getUsername() {
         return username;
     }
+    public String getRealName(){
+        return realName;
+    }
+
 }
