2a79488f40e4a94395fdc84167db0200e8f922e2
lasana941 <k.czechowicz941@gmail.com>
2017-10-11 22:06:15 +0200

    Update OddNumbersExterminator.java
    Wszystko jest ok, napisałam jakiś mały komentarz plus informację jak by w Javie 8 mogło to wyglądać :)

--- k-testing/src/main/java/com/k/testing/collection/OddNumbersExterminator.java
+++ k-testing/src/main/java/com/k/testing/collection/OddNumbersExterminator.java
@@ -5,10 +5,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class OddNumbersExterminator {
+    // korzystając z dobrodziejstw Javy 8 mozesz zrobic:
+    // public List<Integer> exterminate(ArrayList<Integer> numbers) {
+    //    return numbers.stream().filter(nr -> nr % 2 == 0).collect(Collectors.toList());
+    // }
+    // o tym będzie w module o Streamach
     public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
         if (numbers == null){
             return null;
         }
+        // List<Integer> even = new ArrayList<>();
         ArrayList<Integer> even = new ArrayList<>();
         for (Integer num : numbers) {
             if (num % 2 == 0) {
